ec909b32464f4c37e1e8ca109ca4cf4f068fb1eb
TodoApp Admin <admin@todoapp.prus.dev>
2025-04-15 00:28:09 +0300

    Migrate badge and avatar components to new naming convention

--- resources/views/components/ui/avatar/index.blade.php
+++ resources/views/components/ui/avatar/index.blade.php
@@ -8,53 +8,6 @@
     'square' => false
 ])
 
-@php
-    $sizeClasses = [
-        'xs' => 'h-6 w-6 text-xs',
-        'sm' => 'h-8 w-8 text-xs',
-        'md' => 'h-10 w-10 text-sm',
-        'lg' => 'h-12 w-12 text-base',
-        'xl' => 'h-16 w-16 text-lg',
-        '2xl' => 'h-20 w-20 text-xl',
-    ];
-    
-    $statusSizes = [
-        'xs' => 'h-1.5 w-1.5',
-        'sm' => 'h-2 w-2',
-        'md' => 'h-2.5 w-2.5',
-        'lg' => 'h-3 w-3',
-        'xl' => 'h-3.5 w-3.5',
-        '2xl' => 'h-4 w-4',
-    ];
-    
-    $statusColors = [
-        'online' => 'bg-green-400',
-        'away' => 'bg-yellow-400',
-        'busy' => 'bg-red-400',
-        'offline' => 'bg-gray-400',
-    ];
-    
-    $statusPositions = [
-        'top-right' => 'top-0 right-0 -mt-1 -mr-1',
-        'top-left' => 'top-0 left-0 -mt-1 -ml-1',
-        'bottom-right' => 'bottom-0 right-0 -mb-1 -mr-1',
-        'bottom-left' => 'bottom-0 left-0 -mb-1 -ml-1',
-    ];
-    
-    $borderRadius = $square ? 'rounded-md' : 'rounded-full';
-    
-    // Get initials if name is provided
-    $initials = '';
-    if ($name) {
-        $words = explode(' ', $name);
-        if (count($words) >= 2) {
-            $initials = strtoupper(substr($words[0], 0, 1) . substr(end($words), 0, 1));
-        } else {
-            $initials = strtoupper(substr($name, 0, 2));
-        }
-    }
-@endphp
-
 <div class="relative inline-block">
     @if($src)
         <img 
