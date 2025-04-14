140ee90006da1d39c676f38d1b97943cac9d4cbe
TodoApp Admin <admin@todoapp.prus.dev>
2025-04-15 00:29:03 +0300

    Migrate icon component to new naming convention (x-ui.icon)

--- resources/views/components/ui/icon.blade.php
+++ resources/views/components/ui/icon.blade.php
@@ -77,10 +77,10 @@ if (is_string($icon)) {
 @if (!empty($iconContent))
     {!! $iconContent !!}
 @elseif (isset($component))
-    <x-dynamic-component :component="$component" :class="$iconClass" {{ $attributes->except(['class', 'icon']) }} />
+    <x-dynamic-component :component="$component" :class="$class" {{ $attributes->except(['class', 'icon']) }} />
 @elseif (is_string($icon))
     {{-- Fallback SVG for icon not found --}}
-    <svg xmlns="http://www.w3.org/2000/svg" {{ $attributes->merge(['class' => $iconClass]) }} viewBox="0 0 24 24" fill="none" stroke="currentColor">
+    <svg xmlns="http://www.w3.org/2000/svg" {{ $attributes->merge(['class' => $class]) }} viewBox="0 0 24 24" fill="none" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
 @else
